2cad5d7dd480d2a955ffa6afec4fc770f75cf40a
24-06970-clynt <24-06970@g.batstate-u.edu.ph>
2025-11-24 15:30:13 +0800

    Update Outpatient.java

--- Outpatient.java
+++ Outpatient.java
@@ -1,3 +1,23 @@
-public class Outpatient {
-    
-}
+// Outpatient.java
+
+public class Outpatient extends Patient {
+    private String appointmentDate;
+
+    public Outpatient (String id, String name, int age, String diagnosis, String appointmentDate) {
+        super(id, name, age, diagnosis);
+        this.appointmentDate = appointment Date;
+    }
+
+@Override
+
+    public void showInfo() {
+        System.out.println("[Outpatient]");
+        System.out.println("ID: " + getId());
+        System.out.println("Name: + getName());
+        System.out.println("Age: " + getAge());
+        System.out.println("Diagnosis: " + getDiagnosis());
+        System.out.println("Appointment Date: + appointmentDate);
+    }
+
+  }
+
