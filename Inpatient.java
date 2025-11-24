c72eb5eecea0727b53e9b02cd57b88c201a2fa04
24-09622-cell <24-09622@g.batstate-u.edu.ph>
2025-11-24 15:44:57 +0800

    Update Inpatient.java

--- Inpatient.java
+++ Inpatient.java
@@ -1,3 +1,19 @@
-public class Inpatient {
-    
+public class Inpatient extends Patient {
+    private String roomNumber;
+
+    public Inpatient(String id, String name, int age, String diagnosis, String roomNumber)
+    {
+        super(id, name, age, diagnosis);
+        this.roomNumber = roomNumber;
+    }
+
+    public void showInfo() {
+        System.out.println("[Inpatient]");
+        System.out.println("ID: " + getId());
+        System.out.println("Name: " + getName());
+        System.out.println("Age: " + getAge());
+        System.out.println("Diagnosis: " + getDiagnosis());
+        System.out.println("Room Number: " + roomNumber);
+    }
 }
+
